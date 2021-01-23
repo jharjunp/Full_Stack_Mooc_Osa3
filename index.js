d9c476bc02ed6f5012f3fd7c760fde01cc5bbe58
jharjunp <Jari.Harjunpaa@iki.fi>
2021-01-23 12:44:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const morgan = require('morgan')
 const cors = require('cors')
 
 app.use(cors())
-app.use(express.static('/build'))
+app.use(express.static('build'))
 app.use(express.json())
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
